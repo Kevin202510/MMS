2c6b1bbee0b217f9bdc9b687ab18920cf6b89483
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-11-17 16:01:52 +0800

    Asd

--- public/MmsSensorsData.php
+++ public/MmsSensorsData.php
@@ -55,7 +55,7 @@ class Sensors{
    $password="";
    $dbname="mmsdb";
 
-        $sql = 'SELECT * FROM sensorsconfigurations WHERE isActive=1';
+        $sql = 'SELECT * FROM sensorsconfigurations WHERE isActive="1"';
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
         // $rowCount = $stmt->rowCount();
