ab7837e0d7c1c2488df86b5bd40de96196d8cf97
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-11-17 11:09:36 +0800

    asd

--- resources/views/MmsSensorsData.blade.php
+++ resources/views/MmsSensorsData.blade.php
@@ -1,99 +1,66 @@
-@extends('layouts.app')
-
-@section('title')
-    MMS
-@endsection
-
-@section('content')
-    <section class="section">
-        <div class="section-header">
-            <h3 class="page__heading">Dashboard</h3>
-        </div>
-        <!-- <form id="samplemessage" method="post" action="{{url('notify')}}">
-        {{csrf_field()}}
-            <input type="text" id="status" name="message">
-            <input type="text" id="sensorname" name="sensor_name">
-        </form> -->
-        <div class="section-body">
-            @include('layouts.dashdata')
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h5><i class="fas fa-leaf" style="color:#22f545;"></i> <span id="config_name"></span></h5>
-                        </div>
-                        <div class="card-body">
-                        <div class="row">
-                            <div class="col-12 col-md-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h4>Temperature Sensor</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        <canvas id="temperatureChart"></canvas>
-                                        <center id="tempstat"></center>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-md-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h4 style="color: #f27e1f;">Humidity Sensor</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        <canvas id="humidityChart"></canvas>
-                                        <center id="humiditystat"></center>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12 col-md-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h4 style="color: #c5c9c9;">Light Sensor</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        <canvas id="lightChart"></canvas>
-                                        <center id="lightstat"></center>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-12 col-md-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h4 style="color: #38fcae;">Carbon Dioxide Sensor</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        <canvas id="co2Chart"></canvas>
-                                        <center id="co2stat"></center>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- <div class="row justify-content-center">
-                            <div class="col-12 col-md-6">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h4 style="color: #62f5ed;">Water Level Sensor</h4>
-                                    </div>
-                                    <div class="card-body">
-                                        <canvas id="waterLevelChart"></canvas>
-                                        <center id="waterlevelstat"></center>
-                                    </div>
-                                </div>
-                            </div>
-                        </div> -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-@endsection
-
-@section('javascript')
-<script type="module" src="{{ asset('js/dashboard/chartData.js') }}"></script>
-@endsection
-
-
+<?php
+    if(!empty($_POST)){
+        $sensorsval=new Sensors();
+        if(isset($_POST['temperature'])){
+            if($temperaturestatusval==1){
+               $tempval = (float)$_POST['temperature'];
+               $tempstat;
+               if($tempval<$temperaturelimitval){
+                   $tempstat = 2;
+               } else if($tempval>$temperaturemaxval){
+                   $tempstat = 0;
+               } else{
+                   $tempstat = 1;
+               }
+               $sensorsval->temperatureInsert($_POST['temperature'],$tempstat);
+           }
+        }
+       
+        if(isset($_POST['humidity'])){
+           if($humiditystatusval==1){
+               $humidityval = (float)$_POST['humidity'];
+               $humiditystat;
+           if($humidityval<$humiditylimitval){
+               $humiditystat = 2;
+           } else if($humidityval>$humiditymaxval){
+               $humiditystat = 0;
+           } else{
+               $humiditystat = 1;
+           }
+           $sensorsval->humidityInsert($_POST['humidity'],$humiditystat);
+       }
+       }
+       
+       if(isset($_POST['lightAmount'])){
+           if($lightstatusval==1){
+               $lightval = (float)$_POST['lightAmount'];
+               $lightstat;
+           if($lightval<$lightlimitval){
+               $lightstat = 2;
+           } else if($lightval>$lightmaxval){
+               $lightstat = 0;
+           } else{
+               $lightstat = 1;
+           }
+           $sensorsval->LightAmountInsert($_POST['lightAmount'],$lightstat);
+       }
+       }
+       
+       if(isset($_POST['co2Amount'])){
+           if($co2statusval==1){
+               $co2val = (float)$_POST['co2Amount'];
+               $co2stat;
+           if($co2val<$co2limitval){
+               $co2stat = 2;
+           } else if($co2val>$co2maxval){
+               $co2stat = 0;
+           } else{
+               $co2stat = 1;
+           }
+           $sensorsval->CO2AmountInsert($_POST['co2Amount'],$co2stat);
+       }
+       }
+       }else{
+        echo "<script>alert('asdas');</script>";
+       }
+?>
