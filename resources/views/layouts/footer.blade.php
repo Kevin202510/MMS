89e11bf8b2f1b417cbe682f0f41101eff6ed05e0
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-11-18 20:31:15 +0800

    asd'

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,4 @@
 <div class="footer-left">
     All rights reserved &copy; {{ date('Y') }}
 </div>
+<strong style="float:right;">Powered By : <a href="https://kevincaluagprofile.my.canva.site/" style=" text-decoration: none;">Kevin Felix Caluag</a></strong>
