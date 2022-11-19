3aa9a8c98bdd8bc4e36818b35eea62125043d1c0
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-11-19 10:18:08 +0800

    asd

--- database/migrations/2022_05_06_030842_create_temperature_table.php
+++ database/migrations/2022_05_06_030842_create_temperature_table.php
@@ -19,7 +19,7 @@ class CreateTemperatureTable extends Migration
             $table->double("temperature", 15, 8);
             $table->integer("status")->default(0);
             $table->softDeletes();
-            $table->timestamp('created_at')->default(Carbon::now(DB::raw('CURRENT_TIMESTAMP')));
+            $table->timestamp('created_at')->default(Carbon::now()->toDateTimeString());
             
         });
     }
