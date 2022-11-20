0ad66e106317e810488640c8923d9c8201380757
KevinCaluag20 <silent.kevin.f.caluag20@gmail.com>
2022-11-20 13:44:09 +0800

    asd

--- public/js/dashboard/notification.js
+++ public/js/dashboard/notification.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
                 $("#curtemp").html(data[0].temperature+" °C");
                 if(data[0].status==0){
     
-                    $("#notificationContainer").html('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].temperature+'°C Temperature is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
+                    $("#notificationContainer").append('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].temperature+'°C Temperature is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
                     
                 }
                 
@@ -30,7 +30,7 @@ $(document).ready(function(){
                 $("#curhum").html(data[0].humidity+" %");
                 if(data[0].status==0){
     
-                    $("#notificationContainer").html('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].humidity+' °C Humidity is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
+                    $("#notificationContainer").append('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].humidity+' °C Humidity is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
                     
                 }
                 
@@ -45,7 +45,7 @@ $(document).ready(function(){
                 $("#curlig").html(data[0].lightsAmount+" lm");
                 if(data[0].status==0){
     
-                    $("#notificationContainer").html('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].lightsAmount+' lm Light is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
+                    $("#notificationContainer").append('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].lightsAmount+' lm Light is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
                     
                 }
                 
@@ -60,7 +60,7 @@ $(document).ready(function(){
                 $("#curco2").html(data[0].carbondioxideAmount+" ppm");
                 if(data[0].status==0){
     
-                    $("#notificationContainer").html('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].carbondioxideAmount+' ppm CarbonDioxide is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
+                    $("#notificationContainer").append('<div class="dropdown-item dropdown-item-unread"><div class="dropdown-item-icon bg-danger text-white"><i class="fas fa-temperature-high"></i></div><div class="dropdown-item-desc">'+data[0].carbondioxideAmount+' ppm CarbonDioxide is to High<div class="time text-primary">'+data[0].date + " | "+ data[0].time+'</div></div></div>');
                     
                 }
                 
